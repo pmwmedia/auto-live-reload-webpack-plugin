d359e4dcea933f24c1890f837c2bb2dc132d54b3
Martin Winandy <2338728+pmwmedia@users.noreply.github.com>
2025-04-13 14:02:52 +0200

    Reformat source files

--- src/index.ts
+++ src/index.ts
@@ -1,61 +1,61 @@
-import {mkdirSync, readFileSync, rmSync, writeFileSync} from "fs";
-import {createServer, Server} from "http";
-import {AddressInfo} from "net";
-import {resolve} from "path";
-import {OpenFile, openSync} from "temp";
-import {Compiler, WebpackPluginInstance} from "webpack";
-import {WebSocketServer} from "ws";
+import { mkdirSync, readFileSync, rmSync, writeFileSync } from 'fs'
+import { createServer, Server } from 'http'
+import { AddressInfo } from 'net'
+import { resolve } from 'path'
+import { OpenFile, openSync } from 'temp'
+import { Compiler, WebpackPluginInstance } from 'webpack'
+import { WebSocketServer } from 'ws'
 
 type Options = { host?: string, port?: number, enabled?: boolean }
 
 class AutoLiveReloadPlugin implements WebpackPluginInstance {
-    private readonly clientFile: OpenFile;
-    private readonly httpServer: Server | null;
-    private readonly webSocketServer: WebSocketServer | null;
+  private readonly clientFile: OpenFile
+  private readonly httpServer: Server | null
+  private readonly webSocketServer: WebSocketServer | null
 
-    constructor(options?: Options) {
-        const tempDirectory = resolve(__dirname, "..", "tmp");
-        mkdirSync(tempDirectory, {recursive: true});
-        this.clientFile = openSync({dir: tempDirectory, prefix: "client-", suffix: ".js"});
-        process.on("SIGINT", () => rmSync(this.clientFile.path, { recursive: true }));
+  constructor(options?: Options) {
+    const tempDirectory = resolve(__dirname, '..', 'tmp')
+    mkdirSync(tempDirectory, { recursive: true })
+    this.clientFile = openSync({ dir: tempDirectory, prefix: 'client-', suffix: '.js' })
+    process.on('SIGINT', () => rmSync(this.clientFile.path, { recursive: true }))
 
-        if (options !== undefined && options.enabled === false) {
-            this.httpServer = null;
-            this.webSocketServer = null;
+    if (options !== undefined && options.enabled === false) {
+      this.httpServer = null
+      this.webSocketServer = null
 
-            const sourceFilePath = resolve(__dirname, "..", "dist", "client-disabled.js");
-            writeFileSync(this.clientFile.fd, readFileSync(sourceFilePath));
-        } else {
-            const host = options === undefined || options.host === undefined ? "localhost" : options.host;
-            const port = options === undefined || options.port === undefined ? 0 : options.port;
+      const sourceFilePath = resolve(__dirname, '..', 'dist', 'client-disabled.js')
+      writeFileSync(this.clientFile.fd, readFileSync(sourceFilePath))
+    } else {
+      const host = options === undefined || options.host === undefined ? 'localhost' : options.host
+      const port = options === undefined || options.port === undefined ? 0 : options.port
 
-            this.httpServer = createServer().listen(port);
-            this.webSocketServer = new WebSocketServer({server: this.httpServer});
+      this.httpServer = createServer().listen(port)
+      this.webSocketServer = new WebSocketServer({ server: this.httpServer })
 
-            const httpAddress = this.httpServer.address() as AddressInfo;
-            const sourceFilePath = resolve(__dirname, "..", "dist", "client-enabled.js");
-            let sourceContent = readFileSync(sourceFilePath).toString();
-            sourceContent = sourceContent.replaceAll("HOST", host);
-            sourceContent = sourceContent.replaceAll("PORT", httpAddress.port.toString());
-            writeFileSync(this.clientFile.fd, sourceContent);
-        }
+      const httpAddress = this.httpServer.address() as AddressInfo
+      const sourceFilePath = resolve(__dirname, '..', 'dist', 'client-enabled.js')
+      let sourceContent = readFileSync(sourceFilePath).toString()
+      sourceContent = sourceContent.replaceAll('HOST', host)
+      sourceContent = sourceContent.replaceAll('PORT', httpAddress.port.toString())
+      writeFileSync(this.clientFile.fd, sourceContent)
     }
+  }
 
-    clientEntryFile(): string {
-        return this.clientFile.path;
-    }
+  clientEntryFile(): string {
+    return this.clientFile.path
+  }
 
-    apply(compiler: Compiler): void {
-        compiler.hooks.done.tap("AutoLiveReloadPlugin", () => {
-            this.webSocketServer?.clients.forEach(socket => socket.send("RELOAD"));
-        });
+  apply(compiler: Compiler): void {
+    compiler.hooks.done.tap('AutoLiveReloadPlugin', () => {
+      this.webSocketServer?.clients.forEach(socket => socket.send('RELOAD'))
+    })
 
-        compiler.hooks.shutdown.tap("AutoLiveReloadPlugin", () => {
-            this.webSocketServer?.close();
-            this.httpServer?.close();
-            rmSync(this.clientFile.path, { recursive: true });
-        });
-    }
+    compiler.hooks.shutdown.tap('AutoLiveReloadPlugin', () => {
+      this.webSocketServer?.close()
+      this.httpServer?.close()
+      rmSync(this.clientFile.path, { recursive: true })
+    })
+  }
 }
 
-export = AutoLiveReloadPlugin;
+export = AutoLiveReloadPlugin
